028acc4c8e1bc6c21d25b323a4d31a9c8e2d04be
bono-94 <bono.94@hotmail.com>
2022-11-26 00:46:27 +0000

    Set up updater of worksheets

--- run.py
+++ run.py
@@ -39,7 +39,6 @@ def get_main_data():
         print(sales_data)
         validate_data(sales_data)
 
-
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -62,19 +61,40 @@ def validate_data(values):
                 f"Exactly 6 values requiered, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"iInvalid data: {e}, please try again \n")
+        print(f"Invalid data: {e}, please try again\n")
         return False
 
     return True
-    
+
+def update_worksheet (data, worksheet):
+    """
+    Receives list of integers to be inserted into a worksheet
+    update specific worksheet with data provided
+    """
+    print(f"Updating {worksheet} worksheet ... \n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} updated successfully \n")
+
+
 def update_sales_worksheet(data):
     """
     Updates sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet ("dashboard")
+    sales_worksheet = SHEET.worksheet("dashboard")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated succesfully . \n")
+
+
+def update_surplus_worksheet(data):
+    """
+    Updates surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("dashboard")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfully . \n")
 
 
 def calculate_surplus_data(sales_row):
@@ -91,7 +111,15 @@ def calculate_surplus_data(sales_row):
     pprint(stock)
     stock_row = stock[-1]
     print(stock_row)
+    print(f"stock row: {stock_row}")
+    print(f"sales row: {sales_row}")
 
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    print(surplus_data)
+    return surplus_data
 
 
 def main():
@@ -102,7 +130,13 @@ def main():
     print(data)
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
+
+    extra fo refractured code replace upper 2 update codes
+        update_worksheet(sales_data, "sales")
+        update_worksheet(new_surplus_data, "surplus")
+
 
 print("Welcome to Life Data Automation")
 main()
